19cd549f7a7de1455f73a915faa0ce39f8e72eb4
AllureTIme <853794270@qq.com>
2018-03-15 19:07:59 +0800

    将字符串转换为整数

---
+++ src/test/java/Test.java
@@ -0,0 +1,9 @@
+import arithstudy.StringToInt;
+
+public class Test {
+    @org.junit.Test
+    public void testChange(){
+        StringToInt stringToInt = new StringToInt();
+        System.out.println(stringToInt.change("dsd551"));
+    }
+}
