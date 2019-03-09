147ae465623f23b4c99ef5e063fdb22041c30d9e
AllureTIme <853794270@qq.com>
2019-03-09 08:56:17 +0800

    二叉树构建

--- src/arithmetic/java/arit/BinaryTree.java
+++ src/arithmetic/java/arit/BinaryTree.java
@@ -5,7 +5,7 @@ package arit;
  * 请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
  * 例如：前序遍历序列｛ 1, 2, 4, 7, 3, 5, 6, 8｝
  * 和中序遍历序列｛4, 7, 2, 1, 5, 3, 8，6}，
- * 重建出下图所示的二叉树并输出它的头结点
+ * 重建出二叉树并输出它的头结点
  */
 public class BinaryTree {
     public static class BinaryTreeNode {
