1aa61b2907762aa3ea123e454fe2549c0d4aea9b
yu-si-jia <853794270@qq.com>
2020-01-20 17:08:39 +0800

    异或思想

---
+++ src/arit/Xor.java
@@ -0,0 +1,45 @@
+package arit;
+
+/**
+ * @author YuSiJia
+ * @date 2020/1/20
+ * @desc 异或算法
+ */
+public class Xor {
+    /**
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，
+     * 其余每个元素均出现两次。找出那个只出现了一次的元素。
+     */
+    public static int getOnce(int[] arr){
+
+    }
+
+
+    /**
+     * 10-19.20
+     * 快速判断一个数是不是2的x次幂
+     */
+
+    public boolean whether(int num) {
+        int result = num & (num - 1);
+        if (result == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 10-17.18
+     * 求一个二进制数有多少个一
+     */
+    int countBit(int num) {
+        int count = 0;
+        while (num != 0) {
+            num = num & (num - 1);
+            count++;
+        }
+        return count;
+    }
+
+
+}
