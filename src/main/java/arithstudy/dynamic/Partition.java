d91f8095d90aa08c333715473cd06d4b682820d9
yusijia6 <yusijia6@xiaomi.com>
2021-03-17 09:51:44 +0800

    131.分割回文串

---
+++ src/main/java/arithstudy/dynamic/Partition.java
@@ -0,0 +1,53 @@
+package arithstudy.dynamic;
+
+import java.util.*;
+
+/**
+ * @author andor
+ * @date 2021/3/8
+ * @desc 131. 分割回文串
+ */
+public class Partition {
+    boolean[][] f;
+    List<List<String>> ret = new ArrayList<List<String>>();
+    List<String> ans = new ArrayList<String>();
+    int n;
+
+    public List<List<String>> partition(String s) {
+        n = s.length();
+        f = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(f[i], true);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                f[i][j] = (s.charAt(i) == s.charAt(j)) && f[i + 1][j - 1];
+            }
+        }
+
+        dfs(s, 0);
+        return ret;
+    }
+
+    public void dfs(String s, int i) {
+        if (i == n) {
+            ret.add(new ArrayList<String>(ans));
+            return;
+        }
+        for (int j = i; j < n; ++j) {
+            if (f[i][j]) {
+                ans.add(s.substring(i, j + 1));
+                dfs(s, j + 1);
+                ans.remove(ans.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Partition partition = new Partition();
+        partition.partition("aab");
+
+    }
+
+}
