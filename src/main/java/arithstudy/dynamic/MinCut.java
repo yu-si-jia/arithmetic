6c2bf41af9a66cbed4a84ced219ef317e641932a
yusijia6 <yusijia6@xiaomi.com>
2021-03-17 09:51:27 +0800

    132.分割回文串2

---
+++ src/main/java/arithstudy/dynamic/MinCut.java
@@ -0,0 +1,40 @@
+package arithstudy.dynamic;
+
+import java.util.Arrays;
+
+/**
+ * @author andor
+ * @date 2021/3/8
+ * @desc 132. 分割回文串 II
+ */
+public class MinCut {
+    public int minCut(String s) {
+        int n = s.length();
+        boolean[][] g = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(g[i], true);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                g[i][j] = s.charAt(i) == s.charAt(j) && g[i + 1][j - 1];
+            }
+        }
+
+        int[] f = new int[n];
+        Arrays.fill(f, Integer.MAX_VALUE);
+        for (int i = 0; i < n; ++i) {
+            if (g[0][i]) {
+                f[i] = 0;
+            } else {
+                for (int j = 0; j < i; ++j) {
+                    if (g[j + 1][i]) {
+                        f[i] = Math.min(f[i], f[j] + 1);
+                    }
+                }
+            }
+        }
+
+        return f[n - 1];
+    }
+}
