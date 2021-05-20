1f81c729733fe248a5ed1cb56a1526c243ee552c
yusijia6 <yusijia6@xiaomi.com>
2021-05-20 09:24:22 +0800

    74

---
+++ src/main/java/arithstudy/arr/SearchMatrix.java
@@ -0,0 +1,25 @@
+package arithstudy.arr;
+
+/**
+ * @author andor
+ * @date 2021/3/30
+ * @desc 74. 搜索二维矩阵
+ */
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int low = 0, high = m * n - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int x = matrix[mid / n][mid % n];
+            if (x < target) {
+                low = mid + 1;
+            } else if (x > target) {
+                high = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
