70142e8484c11fe2c0d5c1e78a9a1bd8acdfc32b
yusijia6 <yusijia6@xiaomi.com>
2021-05-20 09:23:57 +0800

    26/80

--- src/main/java/arithstudy/arr/RemoveDuplicates.java
+++ src/main/java/arithstudy/arr/RemoveDuplicates.java
@@ -4,6 +4,7 @@ package arithstudy.arr;
  * @author andor
  * @date 2021/3/23
  * @desc 26. 删除有序数组中的重复项
+ *       80. 删除有序数组中的重复项 II
  */
 public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
@@ -19,9 +20,24 @@ public class RemoveDuplicates {
         return nums.length==0?0:fast;
     }
 
+    public int removeDuplicates2(int[] nums) {
+        if (nums.length<2){
+            return nums.length;
+        }
+        int slow = 0;
+        int fast = slow;
+        int count = 0;
+        while (fast<nums.length){
+
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,4,5,6};
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        removeDuplicates.removeDuplicates(nums);
+        int i=2;
+        System.out.println(i++);
+//        removeDuplicates.removeDuplicates(nums);
     }
 }
