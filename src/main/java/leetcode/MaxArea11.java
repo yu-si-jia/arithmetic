031f1a54e406f53b01abbd0a1f31d2ff852d9790
yusijia6 <yusijia6@xiaomi.com>
2021-05-21 09:50:22 +0800

    11. 盛最多水的容器

---
+++ src/main/java/leetcode/MaxArea11.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * @author andor
+ * @date 2021/5/20
+ * @desc 11. 盛最多水的容器
+ */
+public class MaxArea11 {
+    public int maxArea(int[] height) {
+        int left = 0;
+
+        int right = height.length-1;
+
+        int result = 0;
+
+        while (left<right){
+            int area = Math.min(height[left], height[right]) * (right - left);
+            result = Math.max(area,result);
+            if (height[left]<height[right]){
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        MaxArea11 maxArea11 = new MaxArea11();
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea11.maxArea(height));
+    }
+
+}
