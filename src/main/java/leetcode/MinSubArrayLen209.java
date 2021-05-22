ac622dcb580598317fe0ae6bae8f984ca09cb7c9
yusijia6 <yusijia6@xiaomi.com>
2021-05-22 10:48:21 +0800

    209. 长度最小的子数组

---
+++ src/main/java/leetcode/MinSubArrayLen209.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+/**
+ * @author andor
+ * @date 2021/5/21
+ * @desc 209. 长度最小的子数组
+ */
+public class MinSubArrayLen209 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        if (n==0){
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= target) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+
+    }
+
+
+    public static void main(String[] args) {
+        MinSubArrayLen209 minSubArrayLen209 = new MinSubArrayLen209();
+        minSubArrayLen209.minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3});
+    }
+}
