4ea1e2e5f00c95e74bf28ef10f5d4b9b62e5b9f9
yusijia6 <yusijia6@xiaomi.com>
2021-06-04 09:40:13 +0800

    26. 删除有序数组中的重复项

---
+++ src/main/java/leetcode/array/RemoveDuplicates26.java
@@ -0,0 +1,27 @@
+package leetcode.array;
+
+/**
+ * @author andor
+ * @date 2021/6/3
+ * @desc 26. 删除有序数组中的重复项
+ */
+public class RemoveDuplicates26 {
+    public static void main(String[] args) {
+        RemoveDuplicates26 removeDuplicates26 = new RemoveDuplicates26();
+        int[] a = {1,2,3,3,5};
+        System.out.println(removeDuplicates26.removeDuplicates(a));
+    }
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        int first = 0;
+        int second = 1;
+        while (second<nums.length){
+            if (nums[first]!=nums[second]){
+                nums[first+1] = nums[second];
+                first++;
+            }
+            second++;
+        }
+        return first+1;
+    }
+}
