69fc419a7f0393e4c69e197f5a0702c8511e1995
yusijia6 <yusijia6@xiaomi.com>
2021-07-08 10:33:35 +0800

    面试题 02.07. 链表相交

---
+++ src/main/java/leetcode/link/GetIntersectionNode0207.java
@@ -0,0 +1,34 @@
+package leetcode.link;
+
+
+/**
+ * @author andor
+ * @date 2021/7/8
+ * @desc 面试题 02.07. 链表相交
+ */
+public class GetIntersectionNode0207 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a!=b){
+            a = a!=null?a.next: headB;
+            b = b!=null?b.next: headA;
+
+        }
+        return a;
+
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+    }
+}
