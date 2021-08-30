ac92b19a5d2c3737e1a03192095287f2660eee5c
yusijia6 <yusijia6@xiaomi.com>
2021-08-30 21:20:14 +0800

    合并两个有序链表 21

--- src/main/java/leetcode/link/AddTwoNumbers_2.java
+++ src/main/java/leetcode/link/AddTwoNumbers_2.java
@@ -46,21 +46,5 @@ public class AddTwoNumbers_2 {
     }
 
 
-//    class ListNode {
-//        int val;
-//        ListNode next;
-//
-//        ListNode() {
-//        }
-//
-//        ListNode(int val) {
-//            this.val = val;
-//        }
-//
-//        ListNode(int val, ListNode next) {
-//            this.val = val;
-//            this.next = next;
-//        }
-//    }
 
 
---
+++ src/main/java/leetcode/link/ListNode.java
@@ -0,0 +1,23 @@
+package leetcode.link;
+
+/**
+ * @author ysj
+ * @date 2021/8/30
+ * @desc
+ */
+public  class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
