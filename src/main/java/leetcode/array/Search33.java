c00d38ecccea51ee56e3eb9a361de6e3dab29612
yusijia6 <yusijia6@xiaomi.com>
2023-01-06 15:54:27 +0800

    33. 搜索旋转排序数组

--- src/main/java/leetcode/array/Search33.java
+++ src/main/java/leetcode/array/Search33.java
@@ -7,11 +7,13 @@ package leetcode.array;
  */
 public class Search33 {
     public static int search(int[] nums, int target) {
-        int mid = 0;
+        int n = nums.length;
+        if (n == 0) return -1;
+        if (n == 1) return nums[0] == target ? 0 : -1;
         int i = 0;
-        int j = nums.length - 1;
+        int j = n - 1;
         while (i <= j) {
-            mid = (i + j) / 2;
+            int mid = (i + j) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
@@ -33,7 +35,7 @@ public class Search33 {
     }
 
     public static void main(String[] args) {
-        int[] nums = {4, 5, 6, 7, 0, 1, 2};
-        search(nums,0);
+        int[] nums = {5, 1, 3};
+        search(nums, 3);
     }
 }
