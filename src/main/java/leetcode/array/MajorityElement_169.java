967dd6b06ed103eb8451d355d414d511642d5670
yusijia6 <yusijia6@xiaomi.com>
2023-03-21 10:19:35 +0800

    169:出现最多的数字

---
+++ src/main/java/leetcode/array/MajorityElement_169.java
@@ -0,0 +1,27 @@
+package leetcode.array;
+
+/**
+ * @author ysj
+ * @date 2023/3/21
+ * @desc 169:出现最多的数字
+ */
+public class MajorityElement_169 {
+    public int majorityElement(int[] nums) {
+        int num = nums[0];
+        int count = 1;
+        for (int i = 1; i< nums.length ; i++){
+            if (nums[i] == num){
+                count++;
+            }else {
+                count--;
+            }
+
+            if (count==0){
+                num = nums[i];
+                count++;
+            }
+        }
+        return num;
+    }
+
+}
