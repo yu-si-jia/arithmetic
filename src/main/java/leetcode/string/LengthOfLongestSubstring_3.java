7312449815adc0a64335f56d2c333631c7a4c062
yusijia6 <yusijia6@xiaomi.com>
2023-03-27 09:00:03 +0800

    3. 无重复字符的最长子串

--- src/main/java/leetcode/string/LengthOfLongestSubstring_3.java
+++ src/main/java/leetcode/string/LengthOfLongestSubstring_3.java
@@ -10,18 +10,20 @@ import java.util.Map;
  */
 public class LengthOfLongestSubstring_3 {
     public static int lengthOfLongestSubstring(String s) {
-        int n = s.length(), ans = 0;
+        int len = s.length();
+        int ans = 0;
         Map<Character, Integer> map = new HashMap<>();
-        for (int end = 0, start = 0; end < n; end++) {
-            char alpha = s.charAt(end);
-            if (map.containsKey(alpha)) {
-                start = Math.max(map.get(alpha), start);
+        for (int end = 0, start = 0; end < len; end++) {
+            char c = s.charAt(end);
+            if (map.containsKey(c)) {
+                start = Math.max(map.get(c), start);
             }
             ans = Math.max(ans, end - start + 1);
             map.put(s.charAt(end), end + 1);
         }
         return ans;
     }
+
 
 
     public static void main(String[] args) {
