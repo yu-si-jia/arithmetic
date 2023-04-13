e525a1e312cf81d06612490e2508f6e787e401d8
yusijia6 <yusijia6@xiaomi.com>
2023-04-13 11:00:23 +0800

    26. 删除有序数组中的重复项

--- src/main/java/leetcode/array/RemoveDuplicates26.java
+++ src/main/java/leetcode/array/RemoveDuplicates26.java
@@ -8,20 +8,22 @@ package leetcode.array;
 public class RemoveDuplicates26 {
     public static void main(String[] args) {
         RemoveDuplicates26 removeDuplicates26 = new RemoveDuplicates26();
-        int[] a = {1,2,3,3,5};
+        int[] a = {1,2,2,3,4};
         System.out.println(removeDuplicates26.removeDuplicates(a));
     }
     public int removeDuplicates(int[] nums) {
-        if(nums == null || nums.length == 0) return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         int first = 0;
         int second = 1;
-        while (second<nums.length){
-            if (nums[first]!=nums[second]){
-                nums[first+1] = nums[second];
+        while (second < nums.length) {
+            if (nums[first] != nums[second]) {
+                nums[first + 1] = nums[second];
                 first++;
             }
             second++;
         }
-        return first+1;
+        return first + 1;
     }
 }
