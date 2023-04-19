47e004790804880d0ae156a789316e8a8007b591
yusijia6 <yusijia6@xiaomi.com>
2023-04-19 09:24:04 +0800

    27. 移除元素

--- src/main/java/leetcode/array/RemoveElement27.java
+++ src/main/java/leetcode/array/RemoveElement27.java
@@ -6,18 +6,6 @@ package leetcode.array;
  * @desc 27. 移除元素
  */
 public class RemoveElement27 {
-    public int removeElement1(int[] nums, int val) {
-        int right = nums.length-1;
-        for (int i = 0; i<nums.length; i++){
-            if (nums[i] == val){
-                int temp = nums[i];
-                nums[i] = nums[right];
-                nums[right] = temp;
-                right--;
-            }
-        }
-        return right;
-    }
 
 
     public int removeElement(int[] nums, int val) {
