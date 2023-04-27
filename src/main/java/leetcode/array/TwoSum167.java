93b9a9be64ebca53dd7f1e2fe4bcbf94c03238f4
yusijia6 <yusijia6@xiaomi.com>
2023-04-27 20:02:40 +0800

    167： 两数之和

--- src/main/java/leetcode/array/TwoSum167.java
+++ src/main/java/leetcode/array/TwoSum167.java
@@ -3,22 +3,23 @@ package leetcode.array;
 /**
  * @author andor
  * @date 2021/5/18
- * @desc
+ * @desc 167： 两数之和
  */
 public class TwoSum167 {
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
         int left = 0;
         int right = numbers.length-1;
-        while (left!=right)
-        if (numbers[left] + numbers[right] == target) {
-            result[0] = left;
-            result[1] = right;
-            return result;
-        }else if (numbers[left] + numbers[right] > target){
-            right--;
-        }else {
-            left++;
+        while (left!=right) {
+            if (numbers[left] + numbers[right] == target) {
+                result[0] = left;
+                result[1] = right;
+                return result;
+            } else if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
         }
         return null;
     }
