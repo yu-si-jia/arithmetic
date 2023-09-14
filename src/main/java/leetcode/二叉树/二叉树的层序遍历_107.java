7e4e418697431738f913ad0607571c15c678cbff
yusijia6 <yusijia6@xiaomi.com>
2023-09-14 10:10:37 +0800

    二叉树的层序遍历_107

---
+++ src/main/java/leetcode/二叉树/二叉树的层序遍历_107.java
@@ -0,0 +1,39 @@
+package leetcode.二叉树;
+
+import java.util.*;
+
+/**
+ * @author ysj
+ * @date 2023/9/13
+ * @desc
+ */
+public class 二叉树的层序遍历_107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null){
+            return Collections.EMPTY_LIST;
+        }
+
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+
+        while (!q.isEmpty()){
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0 ; i < size; i++){
+                TreeNode cur = q.poll();
+                level.add(cur.val);
+                if (cur.left != null){
+                    q.offer(cur.left);
+                }
+                if (cur.right != null){
+                    q.offer(cur.right);
+                }
+            }
+            result.add(0,level);
+        }
+        return result;
+
+    }
+}
