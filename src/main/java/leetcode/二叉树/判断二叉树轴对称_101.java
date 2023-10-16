0e43a7f10f1b5f3cb9078f225730f33f4003c3b1
hejiageng <hejiageng@kanyun.com>
2023-10-16 22:29:46 +0800

    判断二叉树轴对称_101

--- src/main/java/leetcode/二叉树/判断二叉树轴对称_101.java
+++ src/main/java/leetcode/二叉树/判断二叉树轴对称_101.java
@@ -1,5 +1,8 @@
 package leetcode.二叉树;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author HeJiageng
  * @date 2023/3/16
@@ -27,5 +30,31 @@ public class 判断二叉树轴对称_101 {
             return false;
         }
         return def(left.left,right.right)&&def(left.right,right.left);
+    }
+
+    public boolean checkSymmetricTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode u = queue.poll();
+            TreeNode v = queue.poll();
+            if (u == null && v == null) {
+                continue;
+            }
+            if (u == null || v == null || u.val != v.val) {
+                return false;
+            }
+
+            queue.offer(u.right);
+            queue.offer(v.left);
+
+            queue.offer(u.left);
+            queue.offer(v.right);
+        }
+        return true;
     }
 }
