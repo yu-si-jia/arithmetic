ad577ac933f3729e7117c4b7a68a5055b2178c79
hejiageng <hejiageng@kanyun.com>
2023-12-25 22:22:09 +0800

    跳跃游戏_55

---
+++ src/main/java/leetcode/动态规划/跳跃游戏_55.java
@@ -0,0 +1,24 @@
+package leetcode.动态规划;
+
+import java.util.List;
+
+/**
+ * @author HeJiageng
+ * @date 2023/12/25
+ * @desc
+ */
+public class 跳跃游戏_55 {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int mostRight = 0;
+        for (int i = 0; i < n; i++) {
+            if (i <= mostRight) {
+                mostRight = Math.max(mostRight, i + nums[i]);
+                if (mostRight >= n - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
