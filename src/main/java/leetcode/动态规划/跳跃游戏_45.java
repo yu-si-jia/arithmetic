b75b38ba377b28aaaecb414001ab9d310124c19b
hejiageng <hejiageng@kanyun.com>
2023-12-28 22:48:09 +0800

    跳跃游戏_45

---
+++ src/main/java/leetcode/动态规划/跳跃游戏_45.java
@@ -0,0 +1,29 @@
+package leetcode.动态规划;
+
+/**
+ * @author HeJiageng
+ * @date 2023/12/28
+ * @desc
+ */
+public class 跳跃游戏_45 {
+    public static int jump(int[] nums) {
+        int step = 0;
+        int len = nums.length;
+        int maxPosition = 0;
+        int end = 0;
+        for (int i = 0; i< len-1; i++){
+            maxPosition = Math.max(maxPosition, i+ nums[i]);
+            if (i == end){
+                end = maxPosition;
+                step++;
+            }
+        }
+        return step;
+    }
+
+    public static void main(String[] args) {
+        int[] test = {2,3,1,1,4};
+        System.out.println(跳跃游戏_45.jump(test));
+
+    }
+}
